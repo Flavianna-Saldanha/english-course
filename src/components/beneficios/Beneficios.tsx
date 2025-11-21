dc50e997d57cbd4fcb60998769937237bbdcc64c
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-11-21 14:10:05 -0300

    alterando tamanho de fonte

--- src/components/beneficios/Beneficios.tsx
+++ src/components/beneficios/Beneficios.tsx
@@ -4,7 +4,7 @@ import { BeneficiosItem } from "./BeneficiosItem";
 export const Beneficios = () => {
   return (
     <div className="mt-80 max-w-4xl mx-auto lg:mt-60">
-      <h1 className="text-3xl uppercase text-center mb-10 mx-6">
+      <h1 className="text-2xl uppercase text-center mb-10 mx-6">
         Benefícios de aprender inglês com a gente
       </h1>
       <div className="flex flex-wrap justify-center gap-10">
